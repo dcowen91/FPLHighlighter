454d5b03795c0f763fe2039804fda1188525868b
Drew Owen <dcowen91@gmail.com>
2015-08-11 23:26:07 -0700

    add page action

---
+++ background.js
@@ -0,0 +1,8 @@
+function checkForValidUrl(tabId, changeInfo, tab) {
+	if (tab.url.indexOf('http://fantasy.premierleague.com') == 0) {
+		chrome.pageAction.show(tabId);
+	}
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
