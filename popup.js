272970fb0c1a2cbfba43c3fee697bd3d771cff92
dcowen91 <dcowen@email.arizona.edu>
2015-08-14 01:07:59 -0700

    js, css

--- popup.js
+++ popup.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-	$(".colorChoiceContainer").click( function() {
-		$(".colorChoices").toggle();
+	$("#teamContainer").click( function() {
+		$("#teamChoices").toggle();
 	});
+	$("#watchContainer").click( function() {
+		$("#watchChoices").toggle();
+	});
+	//TODO: Fade in
+
+	//TODO: clickable color choices
+
+	//TODO: add color choice colors
+
+	//TODO: make color choices take effect
+
+	//TODO: save colors to local store
+
+	//TODO: popup.css & clean up
 });
