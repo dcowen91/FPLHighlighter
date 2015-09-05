5f1b93a0df87e4744d1babcc6893d40e511bd45e
dcowen91 <dcowen@email.arizona.edu>
2015-09-04 23:07:01 -0700

    add watchlist

--- PLScript.js
+++ PLScript.js
@@ -1,42 +1,62 @@
 var _PLCOLOR = "yellow";
+var _WATCHCOLOR = "red";
+var _teamIds;
+var _watchIds;
 //TODO: pull this from local store
 
-function collectTeamMembers() {
-	var anchors =  $(".ismDataOriginal a.ismViewProfile");
-	return $.map(anchors, function(value) { 
-		var index = value.href.indexOf("#");
-		return value.href.substring(index);
-	});
+function collectTeamMembers() 
+{
+	var ids = $('#ismJson').metadata().elements;
+	return ids;
 }
+
+function collectWatchList()
+{
+	var ids = $('#ismJson').metadata().watchlist;
+	return ids;
+}
+
 
 function waitAndShowPlayers()
 {
-	window.setTimeout(function() {
-		tagPlayers();
-	}, 1000);
+	window.setTimeout(function() 
+	{
+		_teamIds = collectTeamMembers();
+		_watchIds = collectWatchList();
+		tagPlayers(_teamIds, _PLCOLOR);
+		tagPlayers(_watchIds, _WATCHCOLOR);
+
+	}, 850);
 }
 
-function tagPlayers() {
+function tagPlayers(ids, color) 
+{
 	console.log(chrome.storage);
-	$.each(_playerIds, function(index, value) {
-		var selector = ".ismAddElement[href='" + value + "']";
-		$(selector).parent().parent().css("background-color", _PLCOLOR);
+	$.each(ids, function(index, value) 
+	{
+		if (!!value)
+		{
+			var selector = ".ismAddElement[href='#" + value + "']";
+			$(selector).parent().parent().css("background-color", color);
+		}
 	});
 }
 
-
-var _playerIds = collectTeamMembers();
 waitAndShowPlayers();
 
 $("#ism").on("elementListDisplayFinish", waitAndShowPlayers);
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    _PLCOLOR = request.text;
-	tagPlayers();
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
+//TODO move this functionality to content script
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) 
+//   {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab.url :
+//                 "from the extension");
+//     _PLCOLOR = request.text;
+// 	tagPlayers();
+//     if (request.greeting == "hello")
+//       sendResponse({farewell: "goodbye"});
+//   });
